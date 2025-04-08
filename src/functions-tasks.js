32ff0739ee227207177dcec488a156abae27f0b6
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-04-08 18:22:56 +0200

    feat: implement memoize function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -117,8 +117,17 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let called = false;
+  let result;
+
+  return function memo() {
+    if (!called) {
+      result = func();
+      called = true;
+    }
+    return result;
+  };
 }
 
 /**

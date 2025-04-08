2153533dbbf7f9eafb3f18e9a9a1fbd4fdcb45c6
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-04-08 18:24:40 +0200

    feat: implement retry function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -145,8 +145,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryFunction(...args) {
+    let lastError;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func(...args);
+      } catch (err) {
+        lastError = err;
+      }
+    }
+    throw lastError;
+  };
 }
 
 /**

5dc591740f5c334b5c96256b78464c4cc692b927
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-04-08 18:27:06 +0200

    feat: implement partialUsingArguments function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -206,8 +206,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  function partiallyApplied(...args2) {
+    return fn(...args1, ...args2);
+  }
+  return partiallyApplied;
 }
 
 /**
